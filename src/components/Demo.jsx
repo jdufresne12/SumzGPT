aa57ef749535cfc7434835c44275d8ee5dbfa9ee
John Dufresne <jdufresne135@gmail.com>
2023-05-09 17:58:10 -0400

    fixes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,80 +1,103 @@
-import { useState, useEffect } from "react"
-import { copy, linkIcon, loader, tick } from '../assets'
-import { useLazyGetSummaryQuery } from '../services/article'
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  const [article, setArticle] = useState({url: '', summary: '',})
+  const [article, setArticle] = useState({ url: "", summary: "" });
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
 
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
-    )
-    if(articlesFromLocalStorage){
-      setAllArticles(articlesFromLocalStorage)
+      localStorage.getItem("articles")
+    );
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
   }, []);
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); {/* Use this in react to prevent from submitting */}
-    const { data } = await getSummary({articleUrl: article.url});
-    if(data?.summary){
-      const newArticle = {...article, summary: data.summary};
+    e.preventDefault();
+    {
+      /* Use this in react to prevent from submitting */
+    }
+
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
       setAllArticles(allArticles);
 
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
 
   const handleCopy = (copyUrl) => {
     setCopied(copyUrl);
     navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000 )
-  }
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
-      <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-          <img 
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img
             src={linkIcon}
-            alt="link_icon"
-            className="absolute left-0 my-2 ml-3 w-5"
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
           />
-          <input 
+
+          <input
             type='url'
-            placeholder="Enter a URL"
+            placeholder='Paste the article link'
             value={article.url}
-            onChange={(e) => setArticle({...article, url: e.target.value})}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
             required
-            className="url_input peer"
+            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
-          <button type='submit' className="submit_btn peer-focus: border-gray-700 peer-focus: text-gray-700">
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
             <p>→</p>
           </button>
         </form>
 
         {/* Browse URL History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
-              className='link_card'
+              className="link_card"
             >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
                   alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
+                  className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                 {item.url}
               </p>
             </div>
@@ -85,8 +108,12 @@ const Demo = () => {
       {/* Display Results */}
       <div className="my-10 max-w-full flex justify-center items-center">
         {isFetching ? (
-          <img src={loader} alt="loader_img" className="w-20 h-20 object-contain"/>
-        ) : error ?(
+          <img
+            src={loader}
+            alt="loader_img"
+            className="w-20 h-20 object-contain"
+          />
+        ) : error ? (
           <p className="font-inter font-bold text-black text-center">
             Well, that wasn't supposed to happen
             <br />
@@ -101,14 +128,16 @@ const Demo = () => {
                 Article <span className="blue_gradient">Summary</span>
               </h2>
               <div className="summary_box">
-                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
               </div>
             </div>
           )
         )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
